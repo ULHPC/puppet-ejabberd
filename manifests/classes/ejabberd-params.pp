a847eaa3edbefbc584be34d3564cad3428975ae7
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2012-08-30 01:46:22 +0200

    [ejabberd] Initial commit
    Signed-off-by: Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>

---
+++ manifests/classes/ejabberd-params.pp
@@ -0,0 +1,192 @@
+# File::      <tt>ejabberd-params.pp</tt>
+# Author::    Hyacinthe Cartiaux (hyacinthe.cartiaux@uni.lu)
+# Copyright:: Copyright (c) 2012 Hyacinthe Cartiaux
+# License::   GPLv3
+#
+# ------------------------------------------------------------------------------
+# = Class: ejabberd::params
+#
+# In this class are defined as variables values that are used in other
+# ejabberd classes.
+# This class should be included, where necessary, and eventually be enhanced
+# with support for more OS
+#
+# == Warnings
+#
+# /!\ Always respect the style guide available
+# here[http://docs.puppetlabs.com/guides/style_guide]
+#
+# The usage of a dedicated param classe is advised to better deal with
+# parametrized classes, see
+# http://docs.puppetlabs.com/guides/parameterized_classes.html
+#
+# [Remember: No empty lines between comments and class definition]
+#
+class ejabberd::params {
+
+    ######## DEFAULTS FOR VARIABLES USERS CAN SET ##########################
+    # (Here are set the defaults, provide your custom variables externally)
+    # (The default used is in the line with '')
+    ###########################################
+
+    # ensure the presence (or absence) of ejabberd
+    $ensure = $ejabberd_ensure ? {
+        ''      => 'present',
+        default => "${ejabberd_ensure}"
+    }
+
+    # The Protocol used. Used by monitor and firewall class. Default is 'tcp'
+    $log_level = $ejabberd_loglevel ? {
+        ''      => '4',
+        default => "${ejabberd_loglevel}",
+    }
+
+    $port_c2s = $ejabberd_port_c2s ? {
+        ''      => '5222',
+        default => "${ejabberd_port_c2s}",
+    }
+    $port_s2s = $ejabberd_port_s2s ? {
+        ''      => '5269',
+        default => "${ejabberd_port_s2s}",
+    }
+    $port_http_admin = $ejabberd_port_http_admin ? {
+        ''      => '5280',
+        default => "${ejabberd_port_http_admin}",
+    }
+
+    $s2s_starttls = $ejabberd_s2s_starttls ? {
+        ''      => 'true',
+        default => "${ejabberd_s2s_starttls}",
+    }
+
+    $s2s_default_policy = $ejabberd_s2s_default_policy ? {
+        ''      => 'allow',
+        default => "${ejabberd_s2s_default_policy}",
+    }
+
+    $certfile_path = $ejabberd_certfile_path ? {
+        ''      => '/etc/ejabberd/ejabberd.pem',
+        default => "${ejabberd_certfile_path}",
+    }
+
+
+    $shaper_c2s = $ejabberd_shaper_c2s ? {
+        ''      => '1000',
+        default => "${ejabberd_shaper_c2s}",
+    }
+
+    $shaper_s2s = $ejabberd_shaper_s2s ? {
+        ''      => '50000',
+        default => "${ejabberd_shaper_s2s}",
+    }
+
+    $max_user_sessions = $ejabberd_max_user_sessions ? {
+        ''      => '10',
+        default => "${ejabberd_max_user_sessions}",
+    }
+
+    $max_user_offline_msg = $ejabberd_max_user_offline_msg ? {
+        ''      => '100',
+        default => "${ejabberd_max_user_offline_msg}",
+    }
+
+    $max_admin_offline_msg = $ejabberd_max_admin_offline_msg ? {
+        ''      => '5000',
+        default => "${ejabberd_max_admin_offline_msg}",
+    }
+
+    $default_lang = $ejabberd_default_lang ? {
+        ''      => 'fr',
+        default => "${ejabberd_default_lang}",
+    }
+
+    $ldap_encrypt = $ejabberd_ldap_encrypt ? {
+        ''      => 'tls',
+        default => "${ejabberd_ldap_encrypt}",
+    }
+
+    $ldap_tls_verify = $ejabberd_ldap_tls_verify ? {
+        ''      => 'soft',
+        default => "${ejabberd_ldap_tls_verify}",
+    }
+
+    $ldap_port = $ejabberd_ldap_port ? {
+        ''      => '636',
+        default => "${ejabberd_ldap_port}",
+    }
+
+    $ldap_search_base = $ejabberd_ldap_search_base ? {
+        ''      => 'dc=uni,dc=lu',
+        default => "${ejabberd_ldap_search_base}",
+    }
+
+    $ldap_deref_aliases = $ejabberd_ldap_deref_aliases ? {
+        ''      => 'never',
+        default => "${ejabberd_ldap_deref_aliases}",
+    }
+
+    $ldap_uid_attr = $ejabberd_ldap_uid_attr ? {
+        ''      => 'uid',
+        default => "${ejabberd_ldap_uid_attr}",
+    }
+
+    $ldap_filter = $ejabberd_ldap_filter ? {
+        ''      => '(objectClass=inetOrgPerson)',
+        default => "${ejabberd_ldap_filter}",
+    }
+
+    $admin = $ejabberd_admin ? {
+        ''      => 'admin',
+        default => "${ejabberd_admin}",
+    }
+
+    $register = $ejabberd_register ? {
+        ''      => 'deny',
+        default => "${ejabberd_register}",
+    }
+
+
+    #### MODULE INTERNAL VARIABLES  #########
+    # (Modify to adapt to unsupported OSes)
+    #######################################
+    # ejabberd packages
+    $packagename = $::operatingsystem ? {
+        default => 'ejabberd',
+    }
+
+    # ejabberd associated services
+    $servicename = $::operatingsystem ? {
+        /(?i-mx:ubuntu|debian)/ => 'ejabberd',
+        default                 => 'ejabberd'
+    }
+
+    # Configuration directory & file
+    $configdir = $::operatingsystem ? {
+        default => "/etc/ejabberd",
+    }
+    $configdir_mode = $::operatingsystem ? {
+        default => '0755',
+    }
+    $configdir_owner = $::operatingsystem ? {
+        default => 'root',
+    }
+    $configdir_group = $::operatingsystem ? {
+        default => 'root',
+    }
+
+    $configfile = $::operatingsystem ? {
+        default => '/etc/ejabberd/ejabberd.cfg',
+    }
+    $configfile_mode = $::operatingsystem ? {
+        default => '0640',
+    }
+    $configfile_owner = $::operatingsystem ? {
+        default => 'root',
+    }
+    $configfile_group = $::operatingsystem ? {
+        default => 'ejabberd',
+    }
+
+
+}
+
---
+++ manifests/classes/ejabberd.pp
@@ -0,0 +1,174 @@
+# File::      <tt>ejabberd.pp</tt>
+# Author::    Hyacinthe Cartiaux (hyacinthe.cartiaux@uni.lu)
+# Copyright:: Copyright (c) 2012 Hyacinthe Cartiaux
+# License::   GPLv3
+#
+# ------------------------------------------------------------------------------
+# = Class: ejabberd
+#
+# Configure and manage ejabberd
+#
+# == Parameters:
+#
+# $ensure:: *Default*: 'present'. Ensure the presence (or absence) of ejabberd
+#
+# == Actions:
+#
+# Install and configure ejabberd
+#
+# == Requires:
+#
+# n/a
+#
+# == Sample Usage:
+#
+#     import ejabberd
+#
+# You can then specialize the various aspects of the configuration,
+# for instance:
+#
+#         class { 'ejabberd':
+#             ensure => 'present'
+#         }
+#
+# == Warnings
+#
+# /!\ Always respect the style guide available
+# here[http://docs.puppetlabs.com/guides/style_guide]
+#
+#
+# [Remember: No empty lines between comments and class definition]
+#
+class ejabberd(
+    $ensure                   = $ejabberd::params::ensure,
+    $log_level                = $ejabberd::params::log_level,
+    $port_c2s                 = $ejabberd::params::port_c2s,
+    $port_s2s                 = $ejabberd::params::port_s2s,
+    $port_http_admin          = $ejabberd::params::port_http_admin,
+    $s2s_starttls             = $ejabberd::params::s2s_starttls,
+    $s2s_default_policy       = $ejabberd::params::s2s_default_policy,
+    $certfile_path            = $ejabberd::params::certfile_path,
+    $shaper_c2s               = $ejabberd::params::shaper_c2s,
+    $shaper_s2s               = $ejabberd::params::shaper_s2s,
+    $max_user_sessions        = $ejabberd::params::max_user_sessions,
+    $max_user_offline_msg     = $ejabberd::params::max_user_offline_msg,
+    $max_admin_offline_msg    = $ejabberd::params::max_admin_offline_msg,
+    $default_lang             = $ejabberd::params::default_lang,
+    $ldap_server,
+    $ldap_encrypt             = $ejabberd::params::ldap_encrypt,
+    $ldap_port                = $ejabberd::params::ldap_port,
+    $ldap_search_base         = $ejabberd::params::ldap_search_base,
+    $ldap_deref               = $ejabberd::params::ldap_deref,
+    $ldap_uid_attr            = $ejabberd::params::ldap_uid_attr,
+    $ldap_filter              = $ejabberd::params::ldap_filter,
+    $admin                    = $ejabberd::params::admin,
+    $register                 = $ejabberd::params::register
+)
+inherits ejabberd::params
+{
+    info ("Configuring ejabberd (with ensure = ${ensure})")
+
+    if ! ($ensure in [ 'present', 'absent' ]) {
+        fail("ejabberd 'ensure' parameter must be set to either 'absent' or 'present'")
+    }
+
+    case $::operatingsystem {
+        debian, ubuntu:         { include ejabberd::debian }
+        default: {
+            fail("Module $module_name is not supported on $operatingsystem")
+        }
+    }
+}
+
+# ------------------------------------------------------------------------------
+# = Class: ejabberd::common
+#
+# Base class to be inherited by the other ejabberd classes
+#
+# Note: respect the Naming standard provided here[http://projects.puppetlabs.com/projects/puppet/wiki/Module_Standards]
+class ejabberd::common {
+
+    # Load the variables used in this module. Check the ejabberd-params.pp file
+    require ejabberd::params
+
+    # Configuration file
+    file { "${ejabberd::params::configdir}":
+        ensure => 'directory',
+        owner  => "${ejabberd::params::configdir_owner}",
+        group  => "${ejabberd::params::configdir_group}",
+        mode   => "${ejabberd::params::configdir_mode}",
+        require => Package['ejabberd'],
+    }
+
+    file { 'ejabberd.conf':
+        path    => "${ejabberd::params::configfile}",
+        owner   => "${ejabberd::params::configfile_owner}",
+        group   => "${ejabberd::params::configfile_group}",
+        mode    => "${ejabberd::params::configfile_mode}",
+        ensure  => "${ejabberd::ensure}",
+        content => template("ejabberd/ejabberd.cfg.erb"),
+        require => [
+                    File["${ejabberd::params::configdir}"],
+                    Package['ejabberd']
+                   ],
+    }
+
+    service { 'ejabberd':
+        name       => "${ejabberd::params::servicename}",
+        enable     => true,
+        ensure     => running,
+        require    => [
+                       Package['ejabberd'],
+                       File["${ejabberd::params::configfile}"]
+                      ],
+    }
+
+}
+
+
+# ------------------------------------------------------------------------------
+# = Class: ejabberd::debian
+#
+# Specialization class for Debian systems
+class ejabberd::debian inherits ejabberd::common {
+
+    if ($::lsbdistid == 'Debian') and ( $::lsbdistcodename == 'squeeze' ) {
+        # If Debian Squeeze, use pinning in order to install ejabberd from Wheezy
+
+        apt::source{'wheezy':
+            content => "deb http://ftp.fr.debian.org/debian/ wheezy main contrib non-free\n",
+        }
+
+        apt::preferences {'wheezy_all':
+           package  => '*',
+           pin      => 'release n=wheezy',
+           priority => 100,
+           require  => Apt::Source['wheezy']
+        }
+
+        apt::preferences {'wheezy_ejabberd2110':
+           package  => 'erlang-asn1 erlang-base erlang-crypto erlang-inets
+             erlang-mnesia erlang-odbc erlang-public-key erlang-runtime-tools
+             erlang-ssl erlang-syntax-tools libltdl7 libodbc1 libsctp1 libtinfo5
+             lksctp-tools ejabberd',
+           pin      => 'release n=wheezy',
+           priority => 1500,
+           require  => Apt::Preferences['wheezy_all']
+        }
+
+        package { 'ejabberd':
+            name    => "${ejabberd::params::packagename}",
+            ensure  => "${ejabberd::ensure}",
+            require => Apt::Preferences['wheezy_ejabberd2110']
+        }
+    } else {
+        package { 'ejabberd':
+            name    => "${ejabberd::params::packagename}",
+            ensure  => "${ejabberd::ensure}",
+        }
+    }
+}
+
+
+
+
