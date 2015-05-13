532bb3c8fbe150fafc7de22e68979a0a0163f481
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2015-05-13 15:07:58 +0200

    Class/file reorganization

---
+++ manifests/common.pp
@@ -0,0 +1,65 @@
+# File::      <tt>common.pp</tt>
+# Author::    Hyacinthe Cartiaux (hyacinthe.cartiaux@uni.lu)
+# Copyright:: Copyright (c) 2012 Hyacinthe Cartiaux
+# License::   GPLv3
+# ------------------------------------------------------------------------------
+# = Class: ejabberd::common
+#
+# Base class to be inherited by the other ejabberd classes
+#
+# Note: respect the Naming standard provided here[http://projects.puppetlabs.com/projects/puppet/wiki/Module_Standards]
+class ejabberd::common {
+
+    # Load the variables used in this module. Check the ejabberd-params.pp file
+    require ejabberd::params
+
+    # Configuration file
+    file { $ejabberd::params::configdir:
+        ensure  => 'directory',
+        owner   => $ejabberd::params::configdir_owner,
+        group   => $ejabberd::params::configdir_group,
+        mode    => $ejabberd::params::configdir_mode,
+        require => Package['ejabberd'],
+    }
+
+    file { $ejabberd::params::configfile:
+        ensure  => $ejabberd::ensure,
+        path    => $ejabberd::params::configfile,
+        owner   => $ejabberd::params::configfile_owner,
+        group   => $ejabberd::params::configfile_group,
+        mode    => $ejabberd::params::configfile_mode,
+        content => template('ejabberd/ejabberd.cfg.erb'),
+        require =>  [
+                    File[$ejabberd::params::configdir],
+                    Package['ejabberd']
+                    ],
+        notify  => Service[$ejabberd::params::servicename]
+    }
+
+    # MUC Log dir
+    file { $ejabberd::params::muc_log_dir:
+        ensure  => 'directory',
+        owner   => $ejabberd::params::muc_log_dir_owner,
+        group   => $ejabberd::params::muc_log_dir_group,
+        mode    => $ejabberd::params::muc_log_dir_mode,
+        require => Package['ejabberd'],
+    }
+
+
+    service { 'ejabberd':
+        ensure  => running,
+        name    => $ejabberd::params::servicename,
+        enable  => true,
+        require => [
+                    Package['ejabberd'],
+                    File[$ejabberd::params::configfile],
+                    File[$ejabberd::params::muc_log_dir],
+                    ],
+    }
+
+    package { 'ejabberd':
+        ensure => $ejabberd::ensure,
+        name   => $ejabberd::params::packagename,
+    }
+
+}
---
+++ manifests/common/debian.pp
@@ -0,0 +1,9 @@
+# File::      <tt>debian.pp</tt>
+# Author::    Hyacinthe Cartiaux (hyacinthe.cartiaux@uni.lu)
+# Copyright:: Copyright (c) 2012 Hyacinthe Cartiaux
+# License::   GPLv3
+# ------------------------------------------------------------------------------
+# = Class: ejabberd::common::debian
+#
+# Specialization class for Debian systems
+class ejabberd::common::debian inherits ejabberd::common { }
