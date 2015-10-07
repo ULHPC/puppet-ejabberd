898b7c6c538ca8caf68b706a6890e576de0ff680
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2015-10-07 17:56:10 +0200

    Cosmetic change (lint)

--- manifests/common.pp
+++ manifests/common.pp
@@ -50,7 +50,7 @@ class ejabberd::common {
         ensure  => running,
         name    => $ejabberd::params::servicename,
         enable  => true,
-        status  => "service $ejabberd::params::servicename live",
+        status  => "service ${ejabberd::params::servicename} live",
         require => [
                     Package['ejabberd'],
                     File[$ejabberd::params::configfile],
